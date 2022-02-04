e1a65b8c4309fcd2c922cb7d3d020e358891fc2a
H9Solutions <paulo.artlab@gmail.com>
2022-02-04 04:27:45 -0300

    parse types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,9 @@
-
-export default function Storage<T>({filename, initialValue, beautyJsonParse}: {
-  filename: string,
-  initialValue?: T,
-  beautyJsonParse?: boolean
-}):  unknown extends T ? any : T
+export default function Storage<T>({
+  filename,
+  initialValue,
+  beautyJsonParse,
+}: {
+  filename: string;
+  initialValue?: T;
+  beautyJsonParse?: boolean;
+}): unknown extends T ? any : T;
